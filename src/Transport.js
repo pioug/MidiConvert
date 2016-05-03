8bb550ebf044ae8b0cd1e97c62d674d76f9fa7cb
Gilles Piou <gilles.piou@gmail.com>
2016-05-03 13:59:12 +0800

    Output instrument codes

--- src/Transport.js
+++ src/Transport.js
@@ -2,7 +2,7 @@ define(function(){
 
 	/**
 	 *  Parse tempo and time signature from the midiJson
-	 *  @param  {Object}  midiJson 
+	 *  @param  {Object}  midiJson
 	 *  @return  {Object}
 	 */
 	return function parseTransport(midiJson){
@@ -16,11 +16,32 @@ define(function(){
 						ret.timeSignature = [datum.numerator, datum.denominator];
 					} else if (datum.subtype === "setTempo"){
 						ret.bpm = 60000000 / datum.microsecondsPerBeat;
+					} else if (datum.subtype === "programChange") {
 					}
-				} 
+				} else if (datum.type === "channel") {
+					if (datum.subtype === "programChange") {
+						ret.instruments = ret.instruments || [];
+						ret.instruments[i] =  datum.channel === 9 ? 0 : datum.programNumber + 1;
+					}
+				}
 			}
 		}
+		ret.instruments = compact(ret.instruments);
 		return ret;
 	};
 
+	function compact(array) {
+	  var index = -1;
+		var length = array ? array.length : 0;
+		var resIndex = 0;
+		var result = [];
+
+	  while (++index < length) {
+	    var value = array[index];
+	    if (value || value === 0) {
+	      result[resIndex++] = value;
+	    }
+	  }
+	  return result;
+	}
 });
