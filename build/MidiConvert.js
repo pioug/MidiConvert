899444042a7824d2fedeef5f8360775126c85de6
Gilles Piou <gilles.piou@gmail.com>
2016-08-01 19:06:29 +0800

    Use reduce function to split type 0 track

--- build/MidiConvert.js
+++ build/MidiConvert.js
@@ -1436,7 +1436,7 @@
     var midiJson = MidiFile(fileBlob);
 
     if (midiJson.header.formatType === 0) {
-      splitType0(midiJson);
+      midiJson.tracks = splitType0(midiJson.tracks[0]);
     }
 
     return {
@@ -1445,35 +1445,29 @@
     };
   }
 
-  function splitType0(midiJson) {
-    var tracksMap = {},
-      absoluteTime = 0,
-      tracks = [],
-      event,
-      channel,
-      prevEvent,
-      i;
+  function splitType0(track) {
+    var absoluteTime = 0,
+      tracksMap = track.reduce(groupByChannel, {});
 
-    for (i = 0; i < midiJson.tracks[0].length; i++) {
-      event = midiJson.tracks[0][i];
-      channel = event.channel || 0;
+    return toArray(tracksMap);
+
+    function groupByChannel(result, event) {
+      var channel = event.channel || 0,
+        prevEvent;
+
+      result[channel] = result[channel] || [];
+      prevEvent = result[channel][result[channel].length - 1];
+      result[channel].push(event);
 
       absoluteTime += event.deltaTime;
       event.absoluteTime = absoluteTime;
 
-      tracksMap[channel] = tracksMap[channel] || [];
-      prevEvent = tracksMap[channel][tracksMap[channel].length - 1];
-      tracksMap[channel].push(event);
+      event.deltaTime = prevEvent ?
+        event.absoluteTime - prevEvent.absoluteTime :
+        event.absoluteTime;
 
-      if (prevEvent) {
-        event.deltaTime = event.absoluteTime - prevEvent.absoluteTime;
-      } else {
-        event.deltaTime = event.absoluteTime;
-      }
+      return result;
     }
-
-    midiJson.tracks = toArray(tracksMap);
-    midiJson.header.trackCount = tracks.length;
   }
 
   return MidiConvert;
